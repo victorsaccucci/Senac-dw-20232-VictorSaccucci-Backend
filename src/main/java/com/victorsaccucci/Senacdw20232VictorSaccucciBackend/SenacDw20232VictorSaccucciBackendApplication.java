6c5eee14acb47eb43b7eb2b3dcfb59fd150f25e8
Victor Saccucci <victorsaccucci@hotmail.com>
2023-08-15 09:47:19 -0300

    inital commit

---
+++ src/main/java/com/victorsaccucci/Senacdw20232VictorSaccucciBackend/SenacDw20232VictorSaccucciBackendApplication.java
@@ -0,0 +1,13 @@
+package com.victorsaccucci.Senacdw20232VictorSaccucciBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacDw20232VictorSaccucciBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacDw20232VictorSaccucciBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/victorsaccucci/Senacdw20232VictorSaccucciBackend/SenacDw20232VictorSaccucciBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victorsaccucci.Senacdw20232VictorSaccucciBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenacDw20232VictorSaccucciBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
